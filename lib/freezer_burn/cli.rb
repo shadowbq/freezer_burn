36f08df708f252379d318e44fafc71a809ae993e
shadowbq <shadowbq@gmail.com>
2015-07-14 23:57:40 -0400

    docopt version fix

--- lib/freezer_burn/cli.rb
+++ lib/freezer_burn/cli.rb
@@ -1,56 +1,44 @@
 require "docopt"
-
-#require 'freezer_burn'
+require 'freezer_burn'
 
 
 module FreezerBurn
 
     class CLI
 
-      def self.invoke
-        self.new
-      end
+        def self.invoke
+            self.new
+        end
 
-      def initialize
-          
+        def initialize
+
 doc = <<DOCOPT
 Example of program with many options using docopt.
 Usage:
-  #{__FILE__} [-hvqrf NAME] [--exclude=PATTERNS]
-                     [--select=ERRORS | --ignore=ERRORS] [--show-source]
-                     [--statistics] [--count] [--benchmark] PATH...
-  #{__FILE__} (--doctest | --testsuite=DIR)
-  #{__FILE__} --version
-Arguments:
-  PATH  destination path
+  ./bin/freezer_selector [-hv ]
+  ./bin/freezer_selector --version
+
 Options:
   -h --help            show this help message and exit
   --version            show version and exit
   -v --verbose         print status messages
-  -q --quiet           report only file names
-  -r --repeat          show all occurrences of the same error
-  --exclude=PATTERNS   exclude files or directories which match these comma
-                       separated patterns [default: .svn,CVS,.bzr,.hg,.git]
-  -f NAME --file=NAME  when parsing directories, only check filenames matching
-                       these comma separated patterns [default: *#{__FILE__}]
-  --select=ERRORS      select errors and warnings (e.g. E,W6)
-  --ignore=ERRORS      skip errors and warnings (e.g. E4,W)
-  --show-source        show source code for each error
-  --statistics         count errors and warnings
-  --count              print total number of errors and warnings to standard
-                       error and set exit code to 1 if total is not null
-  --benchmark          measure processing speed
-  --testsuite=DIR      run regression tests from dir
-  --doctest            run doctest on myself
 DOCOPT
 
+            options = {}
             begin
-              require "pp"
-              pp Docopt::docopt(doc)
+                options = Docopt::docopt(doc)
             rescue Docopt::Exit => e
-              puts e.message
+                puts e.message
+                exit 1
             end
-        end
+
+            if options['--version']
+                puts FreezerBurn::VERSION
+                exit 0
+            end
+
+        end # def
+
     end # class
 end # module
 
