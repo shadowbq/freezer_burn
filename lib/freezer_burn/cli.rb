ee707704f9a829726cbac44aa6c1a61d55056ea9
shadowbq <shadowbq@gmail.com>
2015-07-15 01:55:51 -0400

    0.0.2 spike

--- lib/freezer_burn/cli.rb
+++ lib/freezer_burn/cli.rb
@@ -15,16 +15,18 @@ module FreezerBurn
 doc = <<DOCOPT
 Example of program with many options using docopt.
 Usage:
-  ./bin/freezer_selector [-vd] [--run]
+  ./bin/freezer_selector [-vdk] [--version] [-f <fridge>] [-F <freezer>]
   ./bin/freezer_selector -h | --help
-  ./bin/freezer_selector --version
 
 Options:
-  --run                explicitly run the freezer_selector
+  -f <fridge>          look for logs here
+  -F <freezer>         store file here
+  -k --keep-files      keep original files in the fridge
   -h --help            show this help message and exit
   --version            show version and exit
   -d --dry-run         simulate without moving files
   -v --verbose         print status messages
+
 DOCOPT
 
             options = {}
@@ -35,11 +37,6 @@ DOCOPT
                 exit 1
             end
 
-            if options['--version']
-                puts FreezerBurn::VERSION
-                exit 0
-            end
-
             if options['--verbose']
                 FreezerBurn::Settings.verbose = true
             else
@@ -48,6 +45,28 @@ DOCOPT
 
             if options['--dry-run']
                 FreezerBurn::Settings.dryrun = true
+            end
+
+            if options['--keep-files']
+                FreezerBurn::Settings.remove_files = ""
+            end
+
+            if options['-f']
+                FreezerBurn::Settings.fridge = options['-f']
+            end
+
+            if options['-F']
+                FreezerBurn::Settings.freezer = options['-F']
+            end
+
+            if options['--version']
+                puts "version => " + FreezerBurn::VERSION.to_s
+                if FreezerBurn::Settings.verbose
+                    FreezerBurn::Settings.list.each do |toggle|
+                        puts "#{toggle} => #{FreezerBurn::Settings.send(toggle)}"
+                    end
+                end
+                exit 0
             end
 
             FreezerBurn::Rotation.rotate(FreezerBurn::Settings.fridge)
